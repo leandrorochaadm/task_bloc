0930675b62b0260f163ef520417a1dc545223f32
LeandroRocha <leandro.brito.3@globalhitss.com.br>
2023-04-06 23:28:16 -0300

    TasksEvent

--- lib/blocs/bloc/tasks_event.dart
+++ lib/blocs/bloc/tasks_event.dart
@@ -3,3 +3,30 @@ part of 'tasks_bloc.dart';
 abstract class TasksEvent extends Equatable {
   const TasksEvent();
 }
+
+class AddTask extends TasksEvent {
+  final TaskModel task;
+
+  const AddTask({required this.task});
+
+  @override
+  List<Object?> get props => [task];
+}
+
+class UpdateTask extends TasksEvent {
+  final TaskModel task;
+
+  const UpdateTask({required this.task});
+
+  @override
+  List<Object?> get props => [task];
+}
+
+class DeleteTask extends TasksEvent {
+  final TaskModel task;
+
+  const DeleteTask({required this.task});
+
+  @override
+  List<Object?> get props => [task];
+}
