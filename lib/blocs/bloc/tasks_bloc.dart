36859d06f006e0fe6c760b66b9f8066eae6a6a08
LeandroRocha <leandro.brito.3@globalhitss.com.br>
2023-04-07 13:43:57 -0300

    TasksBloc: delete task

--- lib/blocs/bloc/tasks_bloc.dart
+++ lib/blocs/bloc/tasks_bloc.dart
@@ -32,5 +32,9 @@ class TasksBloc extends Bloc<TasksEvent, TasksState> {
     emit(TasksState(allTasks: allTasks));
   }
 
-  void _onDeleteTask(DeleteTask event, Emitter<TasksState> emit) {}
+  void _onDeleteTask(DeleteTask event, Emitter<TasksState> emit) {
+    final state = this.state;
+
+    emit(TasksState(allTasks: List.from(state.allTasks)..remove(event.task)));
+  }
 }
